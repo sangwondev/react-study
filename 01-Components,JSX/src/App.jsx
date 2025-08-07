eba1a7ac4fe03fe462acf53daadd596e14047fc6
sangwondev <sangwonh34@gmail.com>
2025-08-07 23:48:39 +0900

    study: recap basic react concept

--- 01-Components,JSX/src/App.jsx
+++ 01-Components,JSX/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <main>
         <CoreConcepts />
-        <Examples /> 
+        <Examples />
       </main>
     </>
   );
--- 01-Components,JSX/src/index.jsx
+++ 01-Components,JSX/src/index.jsx
@@ -6,4 +6,5 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+
 // ReactDOM.createRoot(entryPoint).render(React.createElement(App));
